8462c2c00c371b7f09c2d553a9ac2e694a0f279d
Juliano Petronetto <juliano.petronetto@gmail.com>
2016-12-28 16:25:43 -0200

    Remoção de itens desnecessários.

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -5,8 +5,8 @@ const routes: Routes = [
   { path: '', component: Layout, children: [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadChildren: () => System.import('../dashboard/dashboard.module') },
-    { path: 'forms', loadChildren: () => System.import('../forms/forms.module') },
-    { path: 'clients', loadChildren: () => System.import('../client/client.module') }
+    { path: 'clients', loadChildren: () => System.import('../client/client.module') },
+    { path: 'equipament', loadChildren: () => System.import('../client/client.module') }
   ]}
 ]
 
