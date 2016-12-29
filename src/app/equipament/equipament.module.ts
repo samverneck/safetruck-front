4ce2a19a3e86bf84a3a17ab93c2ccb54470d825d
Juliano Petronetto <juliano.petronetto@gmail.com>
2016-12-29 21:36:30 -0200

    Finalização de cadastro de equipamento

--- src/app/equipament/equipament.module.ts
+++ src/app/equipament/equipament.module.ts
@@ -7,16 +7,22 @@ import { WidgetModule } from '../layout/widget/widget.module'
 import { UtilsModule } from '../layout/utils/utils.module'
 
 // libs
+import { NKDatetimeModule } from 'ng2-datetime/ng2-datetime'
+import { EquipamentRegisterPage } from './register/register.component'
+import { EquipamentSearchPage } from './search/search.component'
 import 'messenger/build/js/messenger.js'
 import 'jasny-bootstrap/docs/assets/js/vendor/holder.js'
 import 'jasny-bootstrap/js/fileinput.js'
 import 'jasny-bootstrap/js/inputmask.js'
-import 'jasny-bootstrap/docs/assets/js/vendor/holder.js'
-import 'jasny-bootstrap/js/fileinput.js'
-import 'jasny-bootstrap/js/inputmask.js'
-
-import { EquipamentRegisterPage } from './register/register.component'
-import { EquipamentSearchPage } from './search/search.component'
+import 'ng2-datetime/src/vendor/bootstrap-datepicker/bootstrap-datepicker.min.js'
+// import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'
+// import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js'
+import 'bootstrap-select/dist/js/bootstrap-select.js'
+// import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js'
+// import 'bootstrap-markdown/js/bootstrap-markdown.js'
+// import 'bootstrap-colorpicker'
+// import 'bootstrap-slider/dist/bootstrap-slider.js'
+// import { TooltipModule, AlertModule, DropdownModule } from 'ng2-bootstrap/ng2-bootstrap'
 
 export const routes = [
   {path: '', redirectTo: 'register', pathMatch: 'full'},
@@ -32,6 +38,7 @@ export const routes = [
   imports: [
     CommonModule,
     FormsModule,
+    NKDatetimeModule,
     WidgetModule,
     UtilsModule,
     RouterModule.forChild(routes),
