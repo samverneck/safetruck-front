3876026313edfb8f7312380a6ec10301da4fd68c
Juliano Petronetto <juliano.petronetto@gmail.com>
2017-01-06 20:31:52 -0200

    Ajustes nas config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ type StoreType = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES, { useHash: false })
+    RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
     ENV_PROVIDERS,
