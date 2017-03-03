f1ee7ce743b1ac316ff47a832ad1fd36637e372d
hoisel <danielhoiselferraz@gmail.com>
2017-03-03 10:06:26 -0300

    refactor(geral): melhora imports de funções do rxjs
    mudança faz com que somente as funções usadas sejam importadas

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { Http, Headers, Response, RequestOptions } from '@angular/http'
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs/Observable'
 import * as decode from 'jwt-decode'
 
 import { User } from '../features/layout/users/shared'
